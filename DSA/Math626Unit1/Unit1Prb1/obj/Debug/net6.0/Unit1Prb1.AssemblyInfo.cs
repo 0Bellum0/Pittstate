d48c141deb37dc849d1497122caa488d126e2a89
0bellum0 <fpguerra00@gmail.com>
2024-09-16 08:25:35 -0500

    commiting changes

--- DSA/Math626Unit1/Unit1Prb1/obj/Debug/net6.0/Unit1Prb1.AssemblyInfo.cs
+++ DSA/Math626Unit1/Unit1Prb1/obj/Debug/net6.0/Unit1Prb1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit1Prb1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c759b21d6882fa60801978db1def583f07675c22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc10b4f7d14c0fab9af82d583ce871b562d6eb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit1Prb1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit1Prb1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
