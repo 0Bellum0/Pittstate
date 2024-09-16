a8ef3df85cd9d06fb514ae235c08e98d981ad451
0bellum0 <fpguerra00@gmail.com>
2024-09-16 08:34:13 -0500

    commit

--- DSA/Math626Unit1/ArrayQueueLib/obj/Debug/net6.0/ArrayQueueLib.AssemblyInfo.cs
+++ DSA/Math626Unit1/ArrayQueueLib/obj/Debug/net6.0/ArrayQueueLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayQueueLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc10b4f7d14c0fab9af82d583ce871b562d6eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48c141deb37dc849d1497122caa488d126e2a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayQueueLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayQueueLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DSA/Math626Unit1/Unit1Prb2/obj/Debug/net6.0/Unit1Prb2.AssemblyInfo.cs
+++ DSA/Math626Unit1/Unit1Prb2/obj/Debug/net6.0/Unit1Prb2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Unit1Prb2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfc10b4f7d14c0fab9af82d583ce871b562d6eb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d48c141deb37dc849d1497122caa488d126e2a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Unit1Prb2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Unit1Prb2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
